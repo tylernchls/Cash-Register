fa7e001759ec914143e0707d1608874e003d302c
Tyler Nichols <tylernchls@gmail.com>
2016-12-07 19:20:22 -1000

    no changes

--- js/cashRegister.js
+++ js/cashRegister.js
@@ -1,2 +1,7 @@
 var myCalc = calculatorModule();
 console.log(myCalc);
+
+var addbtn = document.getElementById("add");
+var add = document.createElement("button");
+add.innerHTML = "add";
+addbtn.appendChild(add);
