5017af1c4987d9faf7637e390e344acaa73a3649
jhoun <jayhoun@gmail.com>
2016-12-07 19:18:13 -1000

    made numbers 0-9 with for loop

--- js/cashRegister.js
+++ js/cashRegister.js
@@ -1,2 +1,20 @@
 var myCalc = calculatorModule();
-console.log(myCalc);
+
+
+(function(){
+
+for (var i = 0; i <= 9; i++) {
+  var btnBox = document.getElementById('btnBox');
+  var  createBtn = document.createElement('button');
+  createBtn.innerHTML = i;
+  createBtn.id = i;
+  createBtn.addEventListener('click', function(event){
+    console.log(event.target.innerHTML);
+  });
+  btnBox.appendChild(createBtn);
+  }
+})();
+
+
+
+
